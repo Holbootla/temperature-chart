3faebfa1b6d0e208ffc61500fc088fe547463565
Nickita Lashch <holbootla@gmail.com>
2021-11-24 15:20:46 +0300

    feat: change metric system to celcius, add label to chart

--- src/features/API/weatherAPI.js
+++ src/features/API/weatherAPI.js
@@ -3,11 +3,14 @@ import axios from 'axios';
 export default class Api {
   constructor(city) {
     this.API_KEY = 'fd6f65748b2fbec9a43a04f79cde91d1';
-    this.API_SOURCE = 'https://api.openweathermap.org/data/2.5/forecast';
+    this.API_SOURCE = 'https://api.openweathermap.org/data/2.5/forecast/';
     this.city = city;
+    this.units = 'metric';
   }
   async getForecast() {
-    const response = await axios.get(`${this.API_SOURCE}?q=${this.city}&appid=${this.API_KEY}`);
+    const response = await axios.get(
+      `${this.API_SOURCE}?q=${this.city}&units=${this.units}&appid=${this.API_KEY}`
+    );
     return response.data;
   }
 }
--- src/features/chart/Chart.js
+++ src/features/chart/Chart.js
@@ -9,22 +9,30 @@ import {
   Tooltip,
   Legend,
   ResponsiveContainer,
+  LabelList,
 } from 'recharts';
 import { selectSearchBar } from '../search-bar/searchBarSlice';
 
 export function Chart() {
   const { forecastData } = useSelector(selectSearchBar);
 
-  useEffect(() => {
-    console.log(forecastData);
+  const data = forecastData.map((el) => {
+    return {
+      date: el.dt_txt,
+      temp: Math.round(el.main.temp).toString(),
+    };
   });
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
 
   return (
     <ResponsiveContainer width={'100%'} height={300}>
       <BarChart
         width={500}
         height={300}
-        data={forecastData}
+        data={data}
         margin={{
           top: 5,
           right: 30,
@@ -33,11 +41,13 @@ export function Chart() {
         }}
       >
         <CartesianGrid strokeDasharray='3 3' />
-        <XAxis dataKey='dt_txt' />
+        <XAxis dataKey='date' />
         <YAxis />
         <Tooltip />
         <Legend />
-        <Bar dataKey='main.temp' name='Temperature C' fill='green' />
+        <Bar dataKey='temp' name='Temperature C&#176;' fill='green'>
+          <LabelList dataKey='temp' position='top' />
+        </Bar>
       </BarChart>
     </ResponsiveContainer>
   );
